87fdfc6ee9625fb4c51f06cb1b9513909a212027
Khushboo Bhattu <Khushboobhattu@gmail.com>
2022-09-28 20:22:56 -0500

    minor modifications before adding code for gain, attenuation and other sliders

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,7 +7,7 @@
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
-*/
+
 class DecibelSlider : public juce::Slider{
 public:
     DecibelSlider() {}
@@ -31,15 +31,17 @@ private:
     
 };
 
-
+ */
 class MainComponent  : public juce::AudioAppComponent,
 public juce::ChangeListener
 {
 public:
     //==============================================================================
     MainComponent()
-        : state (Stopped)
+        : juce::AudioAppComponent(deviceManager), state (Stopped)
     {
+        
+        //labels
         addAndMakeVisible(titleLabel);
         titleLabel.setFont (juce::Font (16.0f, juce::Font::bold));
         titleLabel.setText ("Play sound from existing file", juce::dontSendNotification);
@@ -52,6 +54,7 @@ public:
         titleLabel_2.setColour (juce::Label::textColourId, juce::Colours::white);
         titleLabel_2.setJustificationType (juce::Justification::centred);
         
+        //buttons & sliders for existing file
         addAndMakeVisible (&openButton);
         openButton.setButtonText ("Open...");
         openButton.onClick = [this] { openButtonClicked(); };
@@ -68,18 +71,15 @@ public:
         stopButton.setColour (juce::TextButton::buttonColourId, juce::Colours::red);
         stopButton.setEnabled (false);
         
-        decibelSlider.setRange (20, 20000);
-        decibelSlider.setTextBoxStyle(juce::Slider::TextBoxRight, false, 100, 20);
-        decibelSlider.onValueChange = [this] {level = juce::Decibels::decibelsToGain ((float) decibelSlider.getValue()); };
-        decibelSlider.setValue (juce::Decibels::gainToDecibels (level));
-        decibelLabel.setText ("Volume in dB", juce::dontSendNotification);
-        
-        addAndMakeVisible (decibelSlider);
-        addAndMakeVisible (decibelLabel);
-        
+        //buttons & sliders for input channels
+
+        deviceManager.initialise(2, 2, nullptr, true);
+        audioSettings.reset(new juce::AudioDeviceSelectorComponent(deviceManager, 0, 2, 0, 2, true, true, true, true));
+        addAndMakeVisible(audioSettings.get());
+
 
         
-        setSize(800, 500);
+        setSize(1000, 500);
         
         formatManager.registerBasicFormats();
         transportSource.addChangeListener (this);   
@@ -101,50 +101,12 @@ public:
 
     void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override
     {
-        auto* device = deviceManager.getCurrentAudioDevice();
-        auto activeInputChannels = device->getActiveInputChannels();
-        auto activeOutputChannels = device->getActiveOutputChannels();
-        auto maxInputChannels = activeInputChannels.getHighestBit() + 1;
-        auto maxOutputChannels = activeOutputChannels.getHighestBit() +1;
+       
+ 
         
-//        auto level = (float) levelSlider.getValue();
-        
-        auto currentLevel = level;
-        auto levelScale = currentLevel * 2.0f;
-        
-        if (readerSource.get() == nullptr){
-            bufferToFill.clearActiveBufferRegion();
-            return;
-        }
+        bufferToFill.clearActiveBufferRegion();
         transportSource.getNextAudioBlock (bufferToFill);
-        
-//        for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel){
-//            auto* buffer = bufferToFill.buffer->getWritePointer (channel, bufferToFill.startSample);
-//
-//            for (auto sample = 0; sample < bufferToFill.numSamples; ++sample) {
-//                buffer[sample] = random.nextFloat() * levelScale - currentLevel;
-//            }
-//        }
-        
-        for (auto channel = 0; channel < maxOutputChannels; ++channel){
-            if ((! activeOutputChannels[channel]) || maxInputChannels == 0){
-                bufferToFill.buffer->clear (channel, bufferToFill.startSample, bufferToFill.numSamples);
-            }
-            else {
-//                auto actualInputChannel = channel % maxInputChannels;
 
-                if(! activeInputChannels[channel]){
-                    bufferToFill.buffer->clear (channel, bufferToFill.startSample, bufferToFill.numSamples);
-                }
-                else{
-                    //auto* inBuffer = bufferToFill.buffer->getReadPointer (actualInputChannel, bufferToFill.startSample);
-                    auto* outBuffer = bufferToFill.buffer->getWritePointer (channel, bufferToFill.startSample);
-                    for (auto sample = 0; sample < bufferToFill.numSamples; ++sample){
-                        outBuffer[sample] = random.nextFloat() * levelScale - currentLevel;
-                    }
-                }
-            }
-        }
     }
     
     void releaseResources() override {
@@ -156,11 +118,10 @@ public:
     {
         titleLabel.setBounds (10, 10, getWidth()/2 - 10, 20);
         titleLabel_2.setBounds (getWidth()/2, 10, getWidth()/2 - 10, 20);
-        openButton.setBounds (10, 40, getWidth()/2 - 10, 20);
-        playButton.setBounds (10, 70, getWidth()/2 - 10, 20);
-        stopButton.setBounds (10, 100, getWidth()/2 - 10, 20);
-        decibelLabel.setBounds (getWidth()/2, 40, getWidth()/2 - 10, 20);
-        decibelSlider.setBounds (getWidth()/2, 70, getWidth()/2 - 10, 20);
+        openButton.setBounds (10, getHeight() - 90, getWidth()/2 - 10, 20);
+        playButton.setBounds (10, getHeight() - 60, getWidth()/2 - 10, 20);
+        stopButton.setBounds (10, getHeight() - 30, getWidth()/2 - 10, 20);
+        audioSettings->setBounds (getWidth()/2, getHeight()/2 - 100, getWidth()/2 - 10, 100);
 
     }
     
@@ -210,7 +171,7 @@ private:
     }
     
     void openButtonClicked(){
-        chooser = std::make_unique<juce::FileChooser> ("Select sound file...", juce::File{}, "*.mp3");
+        chooser = std::make_unique<juce::FileChooser> ("Select sound file...", juce::File{}, "*.mp3; *.wav");
         
         auto chooserFlags = juce::FileBrowserComponent::openMode
         | juce::FileBrowserComponent::canSelectFiles;
@@ -242,14 +203,13 @@ private:
     juce::TextButton playButton;
     juce::TextButton stopButton;
     
-    juce::Random random;
-    DecibelSlider decibelSlider;
-    juce::Label decibelLabel;
-    float level = 0.0f;
 
     
     std::unique_ptr<juce::FileChooser> chooser;
     
+    juce::AudioDeviceManager deviceManager;
+    std::unique_ptr<juce::AudioDeviceSelectorComponent> audioSettings;
+
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
